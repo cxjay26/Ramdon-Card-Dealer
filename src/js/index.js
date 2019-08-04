92d57719bba10addb8fd2d7e83c22200d3852a44
cxjay26 <christianv042393@gmail.com>
2019-08-04 23:41:48 +0000

    finished project

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,48 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  document.querySelector("button").addEventListener("click", () => {
+    var x = document.querySelector("#topsuit");
+    var y = document.querySelector("#bottomsuit");
+    var suit = cardSuit();
+    x.innerHTML = suit;
+    y.innerHTML = suit;
+
+    if (suit === "\u2665") {
+      document.body.style.color = "red";
+    } else if (suit === "\u2666") {
+      document.body.style.color = "red";
+    } else {
+      document.body.style.color = "black";
+    }
+    document.querySelector("#cardvalue").innerHTML = cardValue();
+  });
+
+  function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  let cardSuit = () => {
+    let suits = ["\u2665", "\u2666", "\u2663", "\u2660"];
+    return suits[getRndInteger(0, 4)];
+  };
+
+  let cardValue = () => {
+    let value = [
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K",
+      "A"
+    ];
+    return value[getRndInteger(0, 13)];
+  };
 };
